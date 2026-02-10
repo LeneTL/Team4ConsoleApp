f570e30c98996fd7ccfd3ff87298025ec760c377
Lene <Lene-tl@hotmail.com>
2026-02-10 09:24:32 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppTeam4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
